061c4c298dd7245287cbbd10bb0e704d8a4f3087
Rinn Mattox <rinnreed@gmail.com>
2022-07-06 20:26:13 -0500

    init commit

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-// TODO: add code here
+window.addEventListener("load", function () {
+
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function (response) {
+        return response.json();
+    }).then(function (json) {
+        const container = document.querySelector('#container');
+        let astronauts = '';
+        for (astronaut in json) {
+            <div>
+                <h3>Astronaut ${json[index].firstName} ${json[index].lastName}</h3>
+
+            </div>
+        }
+
+        container.innerHTML.astronauts;
+
+        <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+
+        //console.log(response);
+        // return response;
+
+    });
+
+});
